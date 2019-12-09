5416f144bb7a84faabbe3555fd5e05dbb3bab82c
SuminGo <jennifer045020@gmail.com>
2019-12-09 11:50:59 -0800

    Add Splash page

--- Memories/Memories/app/src/main/java/com/example/memories/MainActivity.kt
+++ Memories/Memories/app/src/main/java/com/example/memories/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //todo : if user is not keeped logged in, go to login page or sign in page
+
+
     }
 }
--- Memories/Memories/app/src/main/java/com/example/memories/SplashActivity.kt
+++ Memories/Memories/app/src/main/java/com/example/memories/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.example.memories
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity(){
 
+    val SPLASH_TIME_OUT : Long = 3000;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            //어떤 액티비티로 넘어 갈지 설정 - 당연히 메인액티비로 넘어 가면 된다.
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+
     }
 }
