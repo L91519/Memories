56be7df01de2dc9de9aed5dae4b7a89ae6668b56
SuminGo <jennifer045020@gmail.com>
2019-12-17 21:18:59 -0800

    recyvler.viewholder init

--- Memories/Memories/app/src/main/java/com/example/memories/CalendarReyclerViewAdapter.kt
+++ Memories/Memories/app/src/main/java/com/example/memories/CalendarReyclerViewAdapter.kt
@@ -4,23 +4,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
-class CalendatReyclerViewAdapter : RecyclerView.Adapter<CalendatReyclerViewAdapter.ViewHolder>()
+class CalendarReyclerViewAdapter(private val items : ArrayList<Date>) : RecyclerView.Adapter<CalendarReyclerViewAdapter.ViewHolder>()
 {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.item_date, parent, false)
+        return CalendarReyclerViewAdapter.ViewHolder(inflatedView)
     }
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        items.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_date, parent, false)) {
+        holder.bind(items[position])
 
     }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item : Date) {}
+    }
+
 }
