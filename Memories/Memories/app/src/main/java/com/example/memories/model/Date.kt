63910a52b8bfa309ed813335d52a5847ad639d2f
Sumin <jennifer045020@gmail.com>
2020-01-20 22:24:42 +0900

    Del

--- Memories/Memories/app/src/main/java/com/example/memories/model/Date.kt
+++
@@ -1,4 +0,0 @@
-package com.example.memories.model
-
-class Date(val date: String, val title : String, val kind : Int) {
-}
--- Memories/Memories/app/src/main/java/com/example/memories/model/Letter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.memories.model
-
-data class Letter(val letter: Char) {
-}
